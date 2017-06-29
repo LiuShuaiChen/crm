0888e5cb7f385bf3c1b76af79cce825e64831a34
liujian8485@outlook.com <liujian68754807>
2017-06-29 09:09:39 +0800

    包名修改

--- crm008/src/com/bjpowernode/crm/security/web/controller/LoginController.java
+++ crm008/src/com/bjpowernode/crm/security/web/controller/LoginController.java
@@ -2,8 +2,6 @@ package com.bjpowernode.crm.security.web.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
--- crm008/src/com/bjpowernode/crm/workbench/activity/dao/MarketActivityDao.java
+++ crm008/src/com/bjpowernode/crm/workbench/activity/dao/MarketActivityDao.java
@@ -2,8 +2,8 @@ package com.bjpowernode.crm.workbench.activity.dao;
 
 import com.bjpowernode.crm.workbench.activity.domain.MarketActivity;
 
-public interface ActivityDao {
+public interface MarketActivityDao {
 	
-	void insert(MarketActivity activity);
+	void insert(MarketActivity marketActivity);
 
 }
